cd3e60b5a09926476b139a246a0b74736d0dc874
jaeyoon <jaeyoon040324@gmail.com>
2024-10-02 04:48:47 +0900

    almost

--- server/settings.py
+++ server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,6 +86,8 @@ CSRF_TRUSTED_ORIGINS = [
     'https://jaeyoon.myddns.me',
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
